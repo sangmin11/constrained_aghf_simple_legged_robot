9983ed2def59bad95a8ff6d1410b606da8f8fb04
Yinai Fan <yinaifan@gmail.com>
2023-07-26 21:46:29 -0700

    remove ignored folder

--- SCA_example/func/plotCircle3D.m
+++
@@ -1,9 +0,0 @@
-function pl = plotCircle3D(center,normal,radius,color,linewidth, grids)
-
-theta=linspace(0,2*pi,grids);
-v=null(normal');
-points=repmat(center,1,size(theta,2))+radius*(v(:,1)*cos(theta)+v(:,2)*sin(theta));
-plot3(points(1,:),zeros(1,length(points(2,:))),points(3,:),color,'LineWidth',linewidth);
-pl = plot3(points(1,:),points(2,:),points(3,:),color,'LineWidth',linewidth);
-
-end
